aa2008a9e757fa2fa2aa7f70708bf3239c93ab6e
Joshua Palathinkal <jpalathinkal@usf.edu>
2021-10-07 16:52:53 -0400

    Add project files.

---
+++ Tech Project 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Project_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
